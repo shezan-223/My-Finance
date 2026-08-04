a63e16b8498cb55d4bf830fc58f9d4504c7db16b
shezan <shezanahmedsourav223@gmail.com>
2026-08-04 16:25:24 +0600

    feat: new pages added in dashboard

--- src/Pages/Dashboard.jsx
+++ src/Pages/Dashboard.jsx
@@ -1,3 +1,6 @@
+import { FaSquarePollVertical } from "react-icons/fa6";
+import { GrTransaction } from "react-icons/gr";
+import { TbTargetArrow } from "react-icons/tb";
 
 const Dashboard = () => {
     return (
@@ -33,6 +36,29 @@ const Dashboard = () => {
             {/* Home icon */}
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" strokeLinejoin="round" strokeLinecap="round" strokeWidth="2" fill="none" stroke="currentColor" className="my-1.5 inline-block size-4"><path d="M15 21v-8a1 1 0 0 0-1-1h-4a1 1 0 0 0-1 1v8"></path><path d="M3 10a2 2 0 0 1 .709-1.528l7-5.999a2 2 0 0 1 2.582 0l7 5.999A2 2 0 0 1 21 10v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path></svg>
             <span className="is-drawer-close:hidden">Homepage</span>
+          </button>
+        </li>
+        <li>
+          <button className="is-drawer-close:tooltip is-drawer-close:tooltip-right" data-tip="Transactions">
+            {/* Home icon */}
+           <i className="text-green-400" ><GrTransaction /></i>
+            <span className="is-drawer-close:hidden text-2xl text-green-400">Transactions</span>
+          </button>
+        </li>
+
+        <li>
+          <button className="is-drawer-close:tooltip is-drawer-close:tooltip-right" data-tip="Reports">
+            {/* Reports icon */}
+           <i className="text-purple-400" ><FaSquarePollVertical /></i>
+            <span className="is-drawer-close:hidden text-2xl text-purple-400">Reports</span>
+          </button>
+        </li>
+        {/* Goals */}
+        <li>
+          <button className="is-drawer-close:tooltip is-drawer-close:tooltip-right" data-tip="Goals">
+            {/* Goals icon */}
+           <i className="text-blue-400" ><TbTargetArrow /></i>
+            <span className="is-drawer-close:hidden text-2xl text-blue-400">Goals</span>
           </button>
         </li>
 
